c82aa81efdb5517422770c70abd67547e678d27f
sinanshamsudheen <carreersins@gmail.com>
2024-11-05 20:23:26 +0530

    nothing

--- numberInfo.py
+++ numberInfo.py
@@ -3,8 +3,8 @@ from phonenumbers import timezone,geocoder,carrier
 
 number=input("enter the number with +__ :")
 phone=phonenumbers.parse(number)
-car=carrier.name_for_number(phone)
+car=carrier.name_for_number(phone,"en")
 zone=timezone.time_zones_for_number(phone)
-loc=geocoder.description_for_number(phone)
+loc=geocoder.description_for_number(phone,"en")
 
 print(f"{phone}\ncarrier: {car}\nTime zone: {zone}\nDescription: {loc}")
