99a9436349ff68dbd3ec3ab053e5a0f8829b3368
sinanshamsudheen <carreersins@gmail.com>
2025-03-16 02:11:04 +0530

    improved output

--- hyperRemove.py
+++ hyperRemove.py
@@ -1,19 +1,18 @@
 #This program will remove all hyperlinks from a pdf
 import fitz
 
-def remove_hyperlinks(input_pdf, output_pdf):
+def remove_hyperlinks(input_pdf):
     doc = fitz.open(input_pdf)
-    
     for page in doc:
         links = page.get_links()
         for link in links:
-            page.delete_link(link)  # Remove each hyperlink
-
-    doc.save(output_pdf)
+            page.delete_link(link)
+    output_path = "D:\IDM\Hyper"
+    list1=input_pdf.split('\\')
+    final_output_path=output_path+"\\"+list1[-1]
+    doc.save(final_output_path)
     doc.close()
-    print(f"Hyperlinks removed. Saved as: {output_pdf}")
+    print(f"Hyperlinks removed. Saved as: {final_output_path}")
 
-# Example usage
 input_pdf = input("enter pdf path: ")
-output_pdf = r"D:\IDM\Hyper\output.pdf"
-remove_hyperlinks(input_pdf, output_pdf)
+remove_hyperlinks(input_pdf)
